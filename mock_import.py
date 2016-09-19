5120a85a6a7bcff67d2ebf7d45a89172ff9828c9
fmoor <frederick.c.moor.v@intel.com>
2016-09-19 16:52:57 -0700

    fix pep8 line too long
    add documentation

--- mock_import.py
+++ mock_import.py
@@ -16,12 +16,29 @@ def _to_list(str_none_or_list):
     return str_none_or_list
 
 
+def _match(module_name, name_to_ignore):
+    """
+    check if `module_name` should be ignored
+
+    :param str|unicode module_name: name that is being imported
+    :param str|unicode name_to_ignore: name that should not be mocked
+    :return: True if the `module_name` should not be mocked
+    :rtype: bool
+    """
+    return (
+        module_name == name_to_ignore
+        or
+        module_name.startswith(name_to_ignore + '.')
+    )
+
+
 def mock_import(do_not_mock=None, **mock_kwargs):
     """
-    Mocks import statement, and disable ImportError if a module
-    could not be imported.
-    :param do_not_mock: a list of prefixes of modules that should
-        exists, and an ImportError could be raised for.
+    Mocks import statements by ignoring ImportErrors
+    and replacing the missing module with a Mock.
+
+    :param str|unicode|list[str|unicode] do_not_mock: names of modules
+        that should exists, and an ImportError could be raised for.
     :param mock_kwargs: kwargs for MagicMock object.
     :return: patch object
     """
@@ -31,11 +48,10 @@ def mock_import(do_not_mock=None, **mock_kwargs):
     def try_import(module_name, *args, **kwargs):
         try:
             return _builtins_import(module_name, *args, **kwargs)
-        except:
-            if any((module_name == prefix or module_name.startswith(prefix + '.')
-                    for prefix in do_not_mock)):
-                # This is a module we need to import, so we don't mock it
-                # and raise the exception
+        except:    # intentionally catch all exceptions
+            if any((_match(module_name, prefix) for prefix in do_not_mock)):
+                # This is a module we need to import,
+                # so we raise the exception instead of mocking it
                 raise
             # Mock external module so we can peacefully create our client
             return mock.MagicMock(**mock_kwargs)
